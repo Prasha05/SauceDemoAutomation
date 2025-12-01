48c5c24bf636b059788b05bb6624e2c2218073fc
Prasha <Prasha@LAPTOP-74A5F175>
2025-12-01 19:51:41 +0530

    Fixed config.properties file path issue

--- SauceDemoAutomation/src/main/java/com/saucedemo/config/ConfigReader.java
+++ SauceDemoAutomation/src/main/java/com/saucedemo/config/ConfigReader.java
@@ -1,18 +1,14 @@
 package com.saucedemo.config;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
 	private static Properties prop;
 	static {
 		try {
-			FileInputStream fis = new FileInputStream(
-					"C:\\Users\\Prasha\\eclipse-workspace\\SauceDemoAutomation\\src\\main\\resources\\config.properties");
 			prop = new Properties();
-			prop.load(fis);
-		} catch (IOException e) {
+			prop.load(ConfigReader.class.getClassLoader().getResourceAsStream("config.properties"));
+		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException("Failed to load config.properties file", e);
 		}
